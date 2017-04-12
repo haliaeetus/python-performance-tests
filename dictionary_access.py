60a41c3e4d4c076cbc76ef1b15451712b0f443bb
Ben Martin <ben@benmartinstudios.com.au>
2017-04-12 13:12:52 +1000

    Added second test with compensation for loop time.

--- dictionary_access.py
+++ dictionary_access.py
@@ -46,6 +46,10 @@ def dict2_selection(data, keys):
     for key in keys:
         data.get(key)
 
+def selection(data, keys):
+    for key in keys:
+        pass
+
 print()
 print("Execution Tests {} random keys (of {})".format(SELECTION_SIZE, SIZE))
 print("""
@@ -54,18 +58,33 @@ print("""
 """.strip())
 selection_keys = list(random.sample(keys, SELECTION_SIZE))
 total = 0
+total_comp = 0
 
 for i in range(1, 100+1):
+    time_loop = timeit.timeit(lambda: selection(data, keys), number=SELECTION_TESTS)
+
     time1 = timeit.timeit(lambda: dict1_selection(data, keys), number=SELECTION_TESTS)
     time2 = timeit.timeit(lambda: dict2_selection(data, keys), number=SELECTION_TESTS)
+
+    time1_comp = time1 - time_loop
+    time2_comp = time2 - time_loop
 
     ratio = time1/time2
     total += ratio
 
-    print("{:<3}: {:.7f} <- {:.7f}".format(i, total/i, ratio))
+    print("{:<3}: {:.7f} <- {:.7f}".format(i, total/i, ratio), end="; ")
+
+    ratio_comp = time1_comp/time2_comp
+    total_comp += ratio_comp
+
+    print("{:<3}: {:.7f} <- {:.7f}".format(i, total_comp/i, ratio_comp))
 
 print()
-print("Direct ran in {:.2f}% of the time of get, on average over {} tests.".format(total, i))
+print("On average, over {} tests: ".format(i))
+print("Uncompensated: ")
+print("  Direct ran in {:.2f}% of the time of get.".format(total))
+print("Compensated for Loop Time: ")
+print("  Direct ran in {:.2f}% of the time of get.".format(total_comp, i))
     
 
 
